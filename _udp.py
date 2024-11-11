ecdfb9a5d48ce5c4e225b4d9adbe2e02ed27fc5a
Beoky <matthiasblokk@gmail.com>
2024-11-11 23:30:27 +0100

    Add files via upload

--- _udp.py
+++ _udp.py
@@ -1,19 +1,5 @@
 print("\033[1;31m"
 
-import os
-import time
-import socket
-import random
-
-#Code Time
-from datetime import datetime
-now = datetime.now()
-hour = now.hour
-minute = now.minute
-day = now.day
-month = now.month
-year = now.year
-
 os.system("clear")
 os.system("figlet UDP")
 
